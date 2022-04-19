37bb67239d71090d3d085d33ae168cf3c209e771
unknown <lefortyop@gmail.com>
2022-04-19 15:42:19 +0200

    add collision direction, enable component

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float MoveSpeed = 8f;
+    GameObject currentFloor;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("hello");
     }
 
     // Update is called once per frame
@@ -27,14 +28,66 @@ public class Player : MonoBehaviour
 
     }
 
-
-    private void OnCollisionEnter(Collision other)
+    void OnCollisionEnter2D(Collision2D other)
     {
+        if (other.gameObject.tag == "Floor")
+        {
+            if (other.contacts[0].normal == new Vector2(0f, 1f))
+            {
+                Debug.Log("you are on floor");
+                currentFloor = other.gameObject;
+            }
+        }
+        else if (other.gameObject.tag == "Fake")
+        {
+            if (other.contacts[0].normal == new Vector2(0f, 1f))
+            {
+                Debug.Log("you are on Fake");
+                currentFloor = other.gameObject;
+            }
+        }
+        else if (other.gameObject.tag == "Nails")
+        {
+            if (other.contacts[0].normal == new Vector2(0f, 1f))
+            {
+                Debug.Log("you are on Nails");
+                currentFloor = other.gameObject;
+            }
+        }
+        else if (other.gameObject.tag == "Trampoline")
+        {
+            if (other.contacts[0].normal == new Vector2(0f, 1f))
+            {
+                Debug.Log("you are on Trampoline");
+                currentFloor = other.gameObject;
+            }
+        }
+        else if (other.gameObject.tag == "Conveyor_Left")
+        {
+            if (other.contacts[0].normal == new Vector2(0f, 1f))
+            {
+                Debug.Log("you are on Conveyor_Left");
+                currentFloor = other.gameObject;
+            }
+        }
+        else if (other.gameObject.tag == "Conveyor_Right")
+        {
+            if (other.contacts[0].normal == new Vector2(0f, 1f))
+            {
 
+                Debug.Log("you are on Conveyor_Right");
+                currentFloor = other.gameObject;
+            }
+        }
+        else if (other.gameObject.tag == "Ceiling")
+        {
+            Debug.Log("You touch the ceiling");
+            currentFloor.GetComponent<BoxCollider2D>().enabled = false;
+        }
 
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "DeathLine")
         {
